d1a204ba7bd3fe33abe3edc78b3846ac177e1c0f
Rashad Ansari <rashad.ansari1996@gmail.com>
2023-04-28 14:23:57 +0200

    Fix CloudWatch alarms

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -6,18 +6,21 @@ resource "aws_sns_topic" "cloudwatch_alarms" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "ec2_cpu_utilization" {
-  count = var.cloudwatch_alarms ? 1 : 0
+  count = var.cloudwatch_alarms ? var.web_server_count : 0
 
   tags                = local.tags
   alarm_name          = "ec2-cpu-utilization"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = 1
+  evaluation_periods  = 2
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
-  period              = 60
+  period              = 120
   statistic           = "Average"
   threshold           = 80
   alarm_description   = "High CPU Utilization (EC2)"
   alarm_actions       = aws_sns_topic.cloudwatch_alarms.*.arn
   ok_actions          = aws_sns_topic.cloudwatch_alarms.*.arn
+  dimensions = {
+    InstanceId = aws_instance.web_servers[count.index].id
+  }
 }
